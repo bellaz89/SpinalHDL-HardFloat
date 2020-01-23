d9d1eb926aaf81c000535f7a139dc933b3e24652
Andrea Bellandi <andrea.bellandi@desy.de>
2020-01-23 15:06:48 +0100

    completed IEEERaw

--- src/main/scala/spinal/lib/float/IEEEtoRaw.scala
+++ src/main/scala/spinal/lib/float/IEEEtoRaw.scala
@@ -1,6 +1,8 @@
 package spinal.lib.float
 
 import spinal.core._
+import spinal.lib._
+import spire.math.pow
 
 object IEEEtoRaw {
 
@@ -9,6 +11,13 @@ object IEEEtoRaw {
     val normalizedDistance = countLeadingZeros(ieee.mantissa) 
     val normalizedMantissa = ((ieee.mantissa<<normalizedDistance)
                               (ieee.mantissaLength-2 to 0) << 1)
+
+    val adjustedExponential = (Mux(ieee.isZero,
+                                   normalizedDistance ^ U(pow(2,ieee.exponentWidth+1)-1),
+                                   U(ieee.exponent)) +
+                                (U(pow(2,ieee.exponentWidth-1)) | Mux(ieee.isExponentZero,
+                                                                      U(2),
+                                                                      U(1))))
 
     val raw = new RawFloat(ieee.mantissaWidth,
                            ieee.exponentWidth)
@@ -20,9 +29,11 @@ object IEEEtoRaw {
     raw.nan := ieee.isNan
     raw.snan := ieee.isSNan
     raw.mantissa(0) := False
-    raw.mantissa(raw.mantissaLength-1 downto 1) := Mux(ieee.isExponentZero, 
+    raw.mantissa(raw.mantissaLength-1 downto 1) := (B(0, 1 bits) ##
+                                                    !ieee.isZero ##
+                                                    Mux(ieee.isExponentZero, 
                                                         normalizedMantissa,
-                                                        ieee.mantissa)
+                                                        ieee.mantissa))
     raw
   }
 }
